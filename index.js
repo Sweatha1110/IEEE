c41efbe27eab7024b12202477b8c5849f4d933b7
thamizhcipher <thamizbharathi22@gmail.com>
2023-08-29 17:34:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// typed js 
+
+var typed = new Typed('#typing-content', {
+    strings: ["Inspire !","Ideate !!", "Innovate !!!",],
+    typeSpeed: 200,
+    backSpeed: 150,
+    showCursor:false,
+    loop:true,
+  });
