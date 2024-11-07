e66587d70754b8431483289edb6652d5a09b558b
sweatha1110 <sweatharam11@gmail.com>
2024-11-07 18:27:39 +0530

    done

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ var typed = new Typed('#typing-content', {
     showCursor:false,
     loop:true,
   });
+
+
+// CLOSE NAV ON CLICK
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   var navLinks = document.querySelectorAll(".navbar-nav>li>a");
+//   navLinks.forEach(function(link) {
+//     link.addEventListener("click", function() {
+//       var navbarCollapse = document.querySelector(".navbar-collapse");
+//       if (navbarCollapse.classList.contains("show")) {
+//         navbarCollapse.classList.remove("show");
+//       }
+//     });
+//   });
+// });
+
+
+// AOS INIT
+
+AOS.init()
